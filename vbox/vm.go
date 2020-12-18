e02f846dde31eecdfc40bbf1504304127e5e074e
Mikalai Davydzenka <mikalai.davydzenka@gmail.com>
2020-12-19 02:25:42 +0300

    implement vms list parser

--- vbox/vm.go
+++ vbox/vm.go
@@ -1,7 +1,10 @@
 package vbox
 
 import (
+	"bufio"
 	"log"
+	"regexp"
+	"strings"
 )
 
 type VM struct {
@@ -9,7 +12,33 @@ type VM struct {
 	UUID string
 }
 
+func reSubMatchMap(r *regexp.Regexp, str string) map[string]string {
+	match := r.FindStringSubmatch(str)
+	subMatchMap := map[string]string{}
+	for i, name := range r.SubexpNames() {
+		if i != 0 {
+			subMatchMap[name] = match[i]
+		}
+	}
+
+	return subMatchMap
+}
+
+var vmsExp = regexp.MustCompile(`"(?P<name>.+)" {(?P<uuid>.+)}`)
+
 func ParseVMsList(vmList string) (vms []VM) {
+	scanner := bufio.NewScanner(strings.NewReader(vmList))
+	for scanner.Scan() {
+		vmMatch := reSubMatchMap(vmsExp, scanner.Text())
+		vms = append(vms, VM{
+			Name: vmMatch["name"],
+			UUID: vmMatch["uuid"],
+		})
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 	return vms
 }
 
--- vbox/vm_test.go
+++ vbox/vm_test.go
@@ -1,6 +1,18 @@
 package vbox
 
-import "testing"
+import (
+	"testing"
+)
+
+func (vm VM) Equal(vmToCmp VM) bool {
+	if vm.Name != vmToCmp.Name {
+		return false
+	}
+	if vm.UUID != vmToCmp.UUID {
+		return false
+	}
+	return true
+}
 
 func TestParseVMsList(t *testing.T) {
 	var testCases = []struct {
@@ -16,11 +28,35 @@ func TestParseVMsList(t *testing.T) {
 				},
 			},
 		},
+		{
+			vmList: `"k8s_node_1" {c67502ef-f0e1-4d2d-909a-6a75bf1f26e5}
+"k8s_node_2" {2233b86a-7935-48a7-8aa3-ded1e3dbaafe}
+"k8s_node_3" {628d6877-6ab1-4d41-b605-aeee24e62670}
+`,
+
+			vms: []VM{
+				VM{
+					Name: "k8s_node_1",
+					UUID: "c67502ef-f0e1-4d2d-909a-6a75bf1f26e5",
+				},
+				VM{
+					Name: "k8s_node_2",
+					UUID: "2233b86a-7935-48a7-8aa3-ded1e3dbaafe",
+				},
+				VM{
+					Name: "k8s_node_3",
+					UUID: "628d6877-6ab1-4d41-b605-aeee24e62670",
+				},
+			},
+		},
 	}
 	for _, testCase := range testCases {
 		got := ParseVMsList(testCase.vmList)
-		if got != testCase.vms {
-			t.Errorf("Abs(-1) = %d; want 1", got)
+		for vmIndex, expectedVM := range testCase.vms {
+			gotVM := got[vmIndex]
+			if !gotVM.Equal(expectedVM) {
+				t.Errorf("Expected %v, but got %v", expectedVM, gotVM)
+			}
 		}
 	}
 }
